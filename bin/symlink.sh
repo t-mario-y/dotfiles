528dbca582a12db6e02213fc7f17572df4cbfbca
t-mario-y <t.mario.yoshi@gmail.com>
2022-05-02 16:21:56 +0900

    fix symlink process(make ~/.config dir)

--- bin/symlink.sh
+++ bin/symlink.sh
@@ -4,5 +4,5 @@ set -eux
 
 ln -snfv ~/dotfiles/.zshrc     ~/.zshrc
 ln -snfv ~/dotfiles/.gitconfig ~/.gitconfig
+mkdir -p ~/.config
 ln -snfv ~/dotfiles/.config/starship.toml ~/.config/starship.toml
-
