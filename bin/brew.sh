949e9c05b7aca6c35d2f7320ff6a511bf058df68
t-mario-y <t.mario.yoshi@gmail.com>
2022-05-02 16:46:11 +0900

    improve fzf install process

--- bin/brew.sh
+++ bin/brew.sh
@@ -5,4 +5,9 @@ set -eux
 brew tap github/gh
 brew tap homebrew/bundle
 brew tap homebrew/core
-brew install asdf awscli coreutils fzf gawk gh ghq git git-delta grep hadolint jq lazygit shellcheck starship tree yq
+
+brew install asdf awscli coreutils gawk gh ghq git git-delta grep hadolint jq lazygit shellcheck starship tree yq
+
+# fzfはキーバインドが必要なので別途インストールする
+brew install fzf
+yes | "$(brew --prefix)"/opt/fzf/install
