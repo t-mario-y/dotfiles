34ed72089ec52dc6b734333c2d64f1870c8ab31c
t-mario-y <t.mario.yoshi@gmail.com>
2022-05-11 23:28:55 +0900

    move .zsh configs to .zsh dir

--- bin/symlink.sh
+++ bin/symlink.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-ln -snfv ~/dotfiles/.zshrc     ~/.zshrc
-ln -snfv ~/dotfiles/.zprofile  ~/.zprofile
+ln -snfv ~/dotfiles/.zsh/.zshrc     ~/.zshrc
+ln -snfv ~/dotfiles/.zsh/.zprofile  ~/.zprofile
 ln -snfv ~/dotfiles/.gitconfig ~/.gitconfig
 ln -snfv ~/dotfiles/.tmux/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config/lazygit
