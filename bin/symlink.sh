58d8ee84f3b064bb3e6d73cd6325fe7978c3ebea
t-mario-y <t.mario.yoshi@gmail.com>
2022-05-11 13:37:29 +0900

    move .tmux.conf

--- bin/symlink.sh
+++ bin/symlink.sh
@@ -5,6 +5,6 @@ set -eu
 ln -snfv ~/dotfiles/.zshrc     ~/.zshrc
 ln -snfv ~/dotfiles/.zprofile  ~/.zprofile
 ln -snfv ~/dotfiles/.gitconfig ~/.gitconfig
-ln -snfv ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -snfv ~/dotfiles/.tmux/.tmux.conf ~/.tmux.conf
 mkdir -p ~/.config
 ln -snfv ~/dotfiles/.config/starship.toml ~/.config/starship.toml
