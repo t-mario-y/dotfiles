00cd0515d1fb8495d666863424b8917b633291b9
t-mario-y <t.mario.yoshi@gmail.com>
2022-05-11 20:39:17 +0900

    add lazygit settings

--- bin/symlink.sh
+++ bin/symlink.sh
@@ -6,5 +6,15 @@ ln -snfv ~/dotfiles/.zshrc     ~/.zshrc
 ln -snfv ~/dotfiles/.zprofile  ~/.zprofile
 ln -snfv ~/dotfiles/.gitconfig ~/.gitconfig
 ln -snfv ~/dotfiles/.tmux/.tmux.conf ~/.tmux.conf
-mkdir -p ~/.config
+mkdir -p ~/.config/lazygit
 ln -snfv ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+
+if [ "$(uname)" = 'Linux' ]; then
+  lazygit_config_file=~/.config/lazygit/config.yml
+fi
+
+if [ "$(uname)" = 'Darwin' ]; then
+  lazygit_config_file=~/Library/Application\ Support/lazygit/config.yml
+fi
+
+ln -snfv ~/dotfiles/.config/lazygit/config.yml "$lazygit_config_file"
