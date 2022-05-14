283cf24f671b0010747a739e44f026da0ebf97b1
t-mario-y <t.mario.yoshi@gmail.com>
2022-05-14 10:58:32 +0900

    add WSL2 + VSCode known issue workaround

--- .zsh/wsl2.zsh
+++ .zsh/wsl2.zsh
@@ -14,3 +14,13 @@ function open() {
     fi
   fi
 }
+
+# WSL2上のVSCode terminalで出る ERROR: UtilConnectToInteropServer:307 対策
+# https://github.com/microsoft/WSL/issues/5065#issuecomment-682198412
+fix_wsl2_interop() {
+  for i in $(pstree -np -s $$ | grep -o -E '[0-9]+'); do
+    if [[ -e "/run/WSL/${i}_interop" ]]; then
+      export WSL_INTEROP=/run/WSL/${i}_interop
+    fi
+done
+}
