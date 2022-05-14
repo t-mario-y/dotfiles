cf3e3fddc96ea1c84c29202cb7c9a2b6618cf8eb
t-mario-y <t.mario.yoshi@gmail.com>
2022-05-15 01:36:08 +0900

    use Array in brew install process

--- bin/install_packages.sh
+++ bin/install_packages.sh
@@ -8,12 +8,33 @@ brew tap homebrew/core
 
 brew upgrade
 
-brew install asdf bat coreutils gawk gh ghq git git-delta grep hadolint jq lazygit shellcheck starship tree yq
+FORMULAE=(
+  awscli
+  asdf
+  bat
+  coreutils
+  gawk
+  gh
+  ghq
+  git
+  git-delta
+  grep
+  hadolint
+  jq
+  lazygit
+  shellcheck
+  starship
+  tree
+  yq
+)
+
+for item in "${FORMULAE[@]}"; do
+  brew install "$item"
+done
+
 # fzfはキーバインドを行うスクリプトを別途インストールするため、処理を分ける。
 brew install fzf
 yes | "$(brew --prefix)"/opt/fzf/install
-# brew installをまとめるとToo many open filesで失敗する。依存が多いawscliを分離して様子を見る。
-brew install awscli
 
 # tmux setup
 brew install tmux
