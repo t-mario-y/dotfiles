c47572e0e01361c55ad82df8bac89a2575cb32fb
t-mario-y <t.mario.yoshi@gmail.com>
2022-05-23 12:19:25 +0900

    add navi cheatsheet

--- bin/symlink.sh
+++ bin/symlink.sh
@@ -6,8 +6,10 @@ ln -snfv ~/dotfiles/.zsh/.zshrc     ~/.zshrc
 ln -snfv ~/dotfiles/.zsh/.zprofile  ~/.zprofile
 ln -snfv ~/dotfiles/.gitconfig ~/.gitconfig
 ln -snfv ~/dotfiles/.tmux/.tmux.conf ~/.tmux.conf
-mkdir -p ~/.config/lazygit
+mkdir -p ~/.config/lazygit ~/.config/navi
 ln -snfv ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+
+ln -snfv ~/dotfiles/navi/config.yaml ~/.config/navi/config.yaml
 
 if [ "$(uname)" = 'Linux' ]; then
   lazygit_config_file=~/.config/lazygit/config.yml
