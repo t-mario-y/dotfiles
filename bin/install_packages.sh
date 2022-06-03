29c06e7cbc9cf2a1bcc14fd647d8789a6fdde4e4
t-mario-y <t.mario.yoshi@gmail.com>
2022-06-04 04:19:41 +0900

    すでにkrewが入っている場合にはインストールしない

--- bin/install_packages.sh
+++ bin/install_packages.sh
@@ -36,13 +36,17 @@ for item in "${FORMULAE[@]}"; do
 done
 
 # krew
-set -x; cd "$(mktemp -d)" &&
-OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
-ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
-KREW="krew-${OS}_${ARCH}" &&
-curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
-tar zxvf "${KREW}.tar.gz" &&
-./"${KREW}" install krew
+if type kubectl krew > /dev/null 2>&1; then
+  echo 'kubectl krew is already installed.'
+else
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+fi
 
 # fzfはキーバインドを行うスクリプトを別途インストールするため、処理を分ける。
 brew install fzf
