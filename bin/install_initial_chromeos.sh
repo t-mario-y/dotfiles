44eeef69b7f0d1b3b904a127d84bf2a40a52c811
t-mario-y <t.mario.yoshi@gmail.com>
2022-08-15 03:19:17 +0900

    fix typo

--- bin/install_initial_chromeos.sh
+++ bin/install_initial_chromeos.sh
@@ -11,14 +11,14 @@ sudo apt-get install -y --no-install-recommends \
   seahorse
 
 # VSCode
-sudo apt-get install wget gpg
+sudo apt-get install -y wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-geet install code
+sudo apt-get install -y code
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
 
