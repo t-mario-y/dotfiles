1bdf7a0cef54140cb5a6c7e1e8b4ee1fc232ad7e
t-mario-y <t.mario.yoshi@gmail.com>
2023-01-02 11:58:54 +0900

    improve lazygit install process

--- bin/install_packages_debian.sh
+++ bin/install_packages_debian.sh
@@ -102,7 +102,6 @@ sudo apt-get install -y \
 sudo usermod -aG docker "$(whoami)"
 
 # lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
-rm -rf lazygit.tar.gz
+asdf plugin add lazygit
+asdf install lazygit 0.36.0
+asdf global lazygit 0.36.0
