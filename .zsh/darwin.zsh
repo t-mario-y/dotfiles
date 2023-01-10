90d4dbfbc74f61dd6dd199d50f3f51e406e0e370
t-mario-y <t.mario.yoshi@gmail.com>
2023-01-11 00:46:14 +0900

    fix brew path on M1 macOS

--- .zsh/darwin.zsh
+++ .zsh/darwin.zsh
@@ -1,3 +1,7 @@
 # macOS共通の.zshrc
 # iTerm
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
+if [ "$(uname -p)" = 'arm' ]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
