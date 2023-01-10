d5509c1419da43dbe50505c8bb32f2b46fca1c9f
t-mario-y <t-mario-y@t-mario-y-game>
2023-01-10 22:17:35 +0900

    migrate docker installation process to another direcotry

--- scripts/install_packages_debian.sh
+++ scripts/install_packages_debian.sh
@@ -79,30 +79,6 @@ asdf plugin add nodejs
 asdf install nodejs 18.12.0
 asdf global nodejs 18.12.0
 
-# docker
-sudo apt-get update
-sudo apt-get install -y \
-  apt-transport-https \
-  ca-certificates \
-  curl \
-  gnupg \
-  lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
-sudo apt-get install -y \
-  docker-ce \
-  docker-ce-cli \
-  containerd.io \
-  docker-compose-plugin
-
-# dockerをsudoなしで実行するためのステップ。ChromeOS & Linuxの再起動が必要。
-sudo usermod -aG docker "$(whoami)"
-
 # lazygit
 asdf plugin add lazygit
 asdf install lazygit 0.36.0
