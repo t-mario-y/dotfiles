7a6a1f3cdc5f3e744a1b47bada941c56d5852c44
t-mario-y <t.mario.yoshi@gmail.com>
2023-01-11 00:48:45 +0900

    install asdf from git, not homebrew

--- scripts/install_and_intialize_homebrew.sh
+++ scripts/install_and_intialize_homebrew.sh
@@ -4,20 +4,20 @@ set -eu
 
 if type brew > /dev/null 2>&1; then
   echo 'homebrew is already installed.'
-  exit 0
+else
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+  if [ "$(uname)" = 'Darwin' ] && [ "$(uname -p)" = 'arm' ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
+
+  brew upgrade
+  brew install gcc
+  brew cleanup
 fi
 
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-if [ "$(uname)" = 'Darwin' ] && [ "$(uname -p)" = 'arm' ]; then
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+if type asdf > /dev/null 2>&1; then
+  echo 'asdf is already installed.'
+else
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.0
 fi
-
-if [ "$(uname)" = 'Linux' ]; then
-  sudo apt-get install build-essential
-  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
-
-brew upgrade
-brew install gcc
-brew cleanup
