2273ed583107f4ccd5a5cfd8fd12d2818eec3438
t-mario-y <t.mario.yoshi@gmail.com>
2023-01-22 18:00:55 +0900

    simply use 'asdf install'

--- scripts/install_packages_common.sh
+++ scripts/install_packages_common.sh
@@ -27,52 +27,13 @@ else
   sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)" -y --force
 fi
 
-if type delta > /dev/null 2>&1; then
-  echo 'delta is already installed.'
-else
-  asdf plugin add delta https://github.com/andweeb/asdf-delta.git
-  asdf install delta 0.15.1
-  asdf global delta 0.15.1
-fi
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs
+asdf plugin add delta https://github.com/andweeb/asdf-delta
+asdf plugin add ghq https://github.com/t-mario-y/asdf-ghq
+asdf plugin add lazygit https://github.com/nklmilojevic/asdf-lazygit
+asdf plugin-add yq https://github.com/beardix/asdf-yq.git
+asdf plugin add hadolint https://github.com/looztra/asdf-hadolint
 
-if type ghq > /dev/null 2>&1; then
-  echo 'ghq is already installed.'
-else
-  asdf plugin add ghq https://github.com/t-mario-y/asdf-ghq
-  asdf install ghq 1.3.0
-  asdf global ghq 1.3.0
-fi
-
-if type node > /dev/null 2>&1; then
-  echo 'node is already installed.'
-else
-  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs
-  asdf install nodejs 18.12.0
-  asdf global nodejs 18.12.0
-fi
-
-if type lazygit > /dev/null 2>&1; then
-  echo 'lazygit is already installed.'
-else
-  asdf plugin add lazygit https://github.com/nklmilojevic/asdf-lazygit
-  asdf install lazygit 0.36.0
-  asdf global lazygit 0.36.0
-fi
-
-if type yq > /dev/null 2>&1; then
-  echo 'yq is already installed.'
-else
-  asdf plugin-add yq https://github.com/beardix/asdf-yq.git
-  asdf install yq v4.30.6
-  asdf global yq v4.30.6
-fi
-
-if type hadolint > /dev/null 2>&1; then
-  echo 'hadolint is already installed.'
-else
-  asdf plugin add hadolint https://github.com/looztra/asdf-hadolint.git
-  asdf install hadolint 2.9.3
-  asdf global hadolint 2.9.3
-fi
+asdf install
 
 npm install -g tldr
