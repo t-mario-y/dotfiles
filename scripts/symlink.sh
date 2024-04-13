224ce89ae327fca288182d9f1d4f5f80719be73e
t-mario-y <t.mario.yoshi@gmail.com>
2024-04-13 21:54:43 +0900

    improve navi config file setup

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -5,21 +5,19 @@ set -eu
 ln -snfv ~/dotfiles/.zsh/.zshrc      ~/.zshrc
 ln -snfv ~/dotfiles/.gitconfig       ~/.gitconfig
 ln -snfv ~/dotfiles/.tmux/.tmux.conf ~/.tmux.conf
-mkdir -p ~/.config
+mkdir -p ~/.config/navi
 ln -snfv ~/dotfiles/.config/starship.toml ~/.config/starship.toml
 
 if [ "$(uname)" = 'Linux' ]; then
-  mkdir -p ~/.config/lazygit ~/.config/navi
+  mkdir -p ~/.config/lazygit
   lazygit_config_file=~/.config/lazygit/config.yml
-  navi_config_file=~/.config/navi/config.yaml
 fi
 
 if [ "$(uname)" = 'Darwin' ]; then
   mkdir -p "$HOME"/Library/Application\ Support/lazygit "$HOME"/Library/Application\ Support/navi
   lazygit_config_file=$HOME/Library/Application\ Support/lazygit/config.yml
-  navi_config_file=$HOME/Library/Application\ Support/navi/config.yaml
 fi
 
 ln -snfv ~/dotfiles/.config/lazygit/config.yml "$lazygit_config_file"
-ln -snfv ~/dotfiles/navi/config.yaml "$navi_config_file"
+ln -snfv ~/dotfiles/navi/config.yaml ~/.config/navi/config.yaml
 ln -snfv ~/dotfiles/.tool-versions ~/.tool-versions
