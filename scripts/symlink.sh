4d440b2dbc4e3f812f5105023ad04fc3e8a4e938
t-mario-y <--global>
2024-04-20 06:55:28 +0900

    add helix editor config

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -7,8 +7,10 @@ ln -snfv ~/dotfiles/.gitconfig       ~/.gitconfig
 ln -snfv ~/dotfiles/.tool-versions   ~/.tool-versions
 ln -snfv ~/dotfiles/.tmux/.tmux.conf ~/.tmux.conf
 ln -snfv ~/dotfiles/starship.toml    ~/.config/starship.toml
-mkdir -p ~/.config/navi
-ln -snfv ~/dotfiles/navi/config.yaml ~/.config/navi/config.yaml
+mkdir -p ~/.config/{helix,navi}
+ln -snfv ~/dotfiles/helix/config.toml    ~/.config/helix/config.toml
+ln -snfv ~/dotfiles/helix/languages.toml ~/.config/helix/languages.toml
+ln -snfv ~/dotfiles/navi/config.yaml     ~/.config/navi/config.yaml
 if [ "$(uname)" = 'Linux' ]; then
   mkdir -p ~/.config/lazygit
   lazygit_config_file=~/.config/lazygit/config.yml
