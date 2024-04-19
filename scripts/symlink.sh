ff79ecde02a9597c2d45036f0fa1f3cad73aa82f
t-mario-y <--global>
2024-04-20 06:36:46 +0900

    fix symlink order

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -4,9 +4,11 @@ set -eu
 
 ln -snfv ~/dotfiles/.zsh/.zshrc      ~/.zshrc
 ln -snfv ~/dotfiles/.gitconfig       ~/.gitconfig
+ln -snfv ~/dotfiles/.tool-versions   ~/.tool-versions
 ln -snfv ~/dotfiles/.tmux/.tmux.conf ~/.tmux.conf
+ln -snfv ~/dotfiles/starship.toml    ~/.config/starship.toml
 mkdir -p ~/.config/navi
-ln -snfv ~/dotfiles/starship.toml ~/.config/starship.toml
+ln -snfv ~/dotfiles/navi/config.yaml ~/.config/navi/config.yaml
 
 if [ "$(uname)" = 'Linux' ]; then
   mkdir -p ~/.config/lazygit
@@ -19,5 +21,3 @@ if [ "$(uname)" = 'Darwin' ]; then
 fi
 
 ln -snfv ~/dotfiles/.config/lazygit/config.yml "$lazygit_config_file"
-ln -snfv ~/dotfiles/navi/config.yaml ~/.config/navi/config.yaml
-ln -snfv ~/dotfiles/.tool-versions ~/.tool-versions
