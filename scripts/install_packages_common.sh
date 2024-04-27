082f0218d8d6f07f8be8a624220d695e93a4f267
t-mario-y <t-mario-y@t-mario-y-game.>
2024-04-27 23:44:31 +0900

    remove ruby management

--- scripts/install_packages_common.sh
+++ scripts/install_packages_common.sh
@@ -28,7 +28,6 @@ else
 fi
 
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs
-asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby
 asdf plugin add delta https://github.com/andweeb/asdf-delta
 asdf plugin add ghq https://github.com/kajisha/asdf-ghq
 asdf plugin add lazygit https://github.com/nklmilojevic/asdf-lazygit
