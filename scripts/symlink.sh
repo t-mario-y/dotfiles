ceef8b6214ed70658ab68f2116849ba9a9d52446
t-mario-y <--global>
2024-05-01 00:50:58 +0900

    add yazi

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-mkdir -p ~/.config/{helix,navi}
+mkdir -p ~/.config/{helix,navi,yazi}
 ln -snfv ~/dotfiles/.zsh/.zshrc      ~/.zshrc
 ln -snfv ~/dotfiles/.gitconfig       ~/.gitconfig
 ln -snfv ~/dotfiles/.tool-versions   ~/.tool-versions
@@ -11,6 +11,7 @@ ln -snfv ~/dotfiles/starship.toml    ~/.config/starship.toml
 ln -snfv ~/dotfiles/helix/config.toml    ~/.config/helix/config.toml
 ln -snfv ~/dotfiles/helix/languages.toml ~/.config/helix/languages.toml
 ln -snfv ~/dotfiles/navi/config.yaml     ~/.config/navi/config.yaml
+ln -snfv ~/dotfiles/yazi/yazi.toml       ~/.config/yazi/yazi.toml
 if [ "$(uname)" = 'Linux' ]; then
   mkdir -p ~/.config/lazygit
   lazygit_config_file=~/.config/lazygit/config.yml
