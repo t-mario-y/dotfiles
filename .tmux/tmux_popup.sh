9966eccf2f13c1a7a9beec4342a0e4c0d8b11134
t-mario-y <--global>
2024-05-02 23:00:57 +0900

    fix nits

--- .tmux/tmux_popup.sh
+++ .tmux/tmux_popup.sh
@@ -5,5 +5,5 @@ height=${2:-60%}
 if [ "$(tmux display-message -p -F "#{session_name}")" = "popup" ];then
   tmux detach-client
 else
-  tmux popup -d '#{pane_current_path}' -xC -yC -w"$width" -h"$height" -E "tmux attach -t popup || tmux new -s popup"
+  tmux popup -d '#{pane_current_path}' -xC -yC -w"$width" -h"$height" -E "tmux attach-session -t popup || tmux new-session -s popup"
 fi
