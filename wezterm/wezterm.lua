59df09665b210fa039c01983cdc9e70fb6e7a2f5
t-mario-y <t.mario.yoshi@gmail.com>
2024-05-03 17:38:28 +0900

    restrict serach

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -15,6 +15,8 @@ local config = {
     -- https://github.com/wez/wezterm/issues/253#issuecomment-672007120
     { key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
     { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
+    -- disable Cmd + F search. TODO: only inside tmux
+    { key = "f",          mods = 'CMD', action = wezterm.action.DisableDefaultAssignment },
   }
 }
 
