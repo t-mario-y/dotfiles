bd9842ece69773cb9ea3af0107b69b2eaa1f9bfa
t-mario-y <t.mario.yoshi@gmail.com>
2024-05-03 16:34:18 +0900

    improve Alt + Right/Left arrows

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,6 +10,12 @@ local config = {
   line_height = 0.85,
   hide_tab_bar_if_only_one_tab = true,
   -- window_background_opacity = 0.8,
+  keys = {
+    -- To capture Alt + Left/Right arrows
+    -- https://github.com/wez/wezterm/issues/253#issuecomment-672007120
+    { key = "LeftArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
+    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
+  }
 }
 
 return config
