dc7c3641c98f90ae61ca4275d2035a9f0dbf74f8
t-mario-y <--global>
2024-08-09 14:44:58 +0900

    add serve_dir.ts

---
+++ utils/serve_dir.ts
@@ -0,0 +1,19 @@
+import { serveDir } from 'jsr:@std/http/file-server';
+
+// PORT=3001 SERVE_DIR=coverage deno run --allow-all ~/dotfiles/utils/serve_dir.ts
+const dir = Deno.env.get('SERVE_DIR');
+Deno.serve(
+  { port: parseInt(Deno.env.get('PORT') || '3001') },
+  (req: Request) => {
+    const pathname = new URL(req.url).pathname;
+    if (pathname.startsWith(`/${dir}`)) {
+      return serveDir(req, {
+        fsRoot: dir,
+        urlRoot: dir,
+      });
+    }
+    return new Response('404: Not Found', {
+      status: 404,
+    });
+  }
+);
