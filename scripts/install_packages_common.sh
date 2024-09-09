51f4baebb85370d8f56ad3e0170202d18f3e24ad
t-mario-y <--global>
2024-09-09 12:59:10 +0900

    divide sections for tools installation

--- scripts/install_packages_common.sh
+++ scripts/install_packages_common.sh
@@ -2,19 +2,7 @@
 
 set -eu
 
-# These commands are easy, but wait user input
 curl -fsSL https://deno.land/install.sh | sh
-curl -fsS  https://sh.rustup.rs | sh
-cargo install --locked \
-  bat \
-  broot \
-  fd-find \
-  git-delta \
-  ripgrep \
-  taplo-cli \
-  yazi-cli \
-  yazi-fm \
-  zoxide
 
 if type fzf > /dev/null 2>&1; then
   echo 'fzf is already installed.'
@@ -29,6 +17,14 @@ else
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
 fi
 
+asdf plugin add ghq https://github.com/kajisha/asdf-ghq
+asdf plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
+asdf plugin add lazygit https://github.com/nklmilojevic/asdf-lazygit
+asdf plugin-add yq https://github.com/beardix/asdf-yq
+
+asdf install
+
+# terminal helpers, not essential
 if type navi > /dev/null 2>&1; then
   echo 'navi is already installed.'
 else
@@ -41,9 +37,14 @@ else
   sudo sh -c "$(curl -fsSL https://starship.rs/install.sh)" -y --force
 fi
 
-asdf plugin add ghq https://github.com/kajisha/asdf-ghq
-asdf plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
-asdf plugin add lazygit https://github.com/nklmilojevic/asdf-lazygit
-asdf plugin-add yq https://github.com/beardix/asdf-yq
-
-asdf install
+curl -fsS  https://sh.rustup.rs | sh
+cargo install --locked \
+  bat \
+  broot \
+  fd-find \
+  git-delta \
+  ripgrep \
+  taplo-cli \
+  yazi-cli \
+  yazi-fm \
+  zoxide
