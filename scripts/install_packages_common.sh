92daa8f55ccf80b4dc9bdfccd83e61ec7f9b08ef
t-mario-y <t.mario.yoshi@gmail.com>
2024-09-15 23:06:30 +0900

    improve install script

--- scripts/install_packages_common.sh
+++ scripts/install_packages_common.sh
@@ -2,11 +2,16 @@
 
 set -eu
 
-curl -fsSL https://deno.land/install.sh | sh
+if type deno > /dev/null 2>&1; then
+  echo 'deno is already installed.'
+else
+  curl -fsSL https://deno.land/install.sh | sh
+fi
 
 if type fzf > /dev/null 2>&1; then
   echo 'fzf is already installed.'
 else
+  rm -rf ~/.fzf
   git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
   yes | ~/.fzf/install
 fi
@@ -14,22 +19,12 @@ fi
 if type asdf > /dev/null 2>&1; then
   echo 'asdf is already installed.'
 else
+  rm -rf ~/.asdf
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
 fi
 
-asdf plugin add ghq https://github.com/kajisha/asdf-ghq
-asdf plugin add lazydocker https://github.com/comdotlinux/asdf-lazydocker.git
-asdf plugin add lazygit https://github.com/nklmilojevic/asdf-lazygit
-asdf plugin-add yq https://github.com/beardix/asdf-yq
-
-asdf install
-
 # terminal helpers, not essential
-if type navi > /dev/null 2>&1; then
-  echo 'navi is already installed.'
-else
-  bash <(curl -sL https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install)
-fi
+bash <(curl -sL https://raw.githubusercontent.com/denisidoro/navi/master/scripts/install)
 
 if type starship > /dev/null 2>&1; then
   echo 'starship is already installed.'
