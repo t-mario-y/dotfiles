9537f14166bbb7048056d80bea3b20be6d02fc59
t-mario-y <yoshiaki-tomoi@c-fo.com>
2025-08-01 14:20:05 +0900

    add deno run serve

--- utils/serve_dir.ts
+++ utils/serve_dir.ts
@@ -1,9 +1,9 @@
 import { serveDir } from "jsr:@std/http/file-server";
 
-// PORT=3001 SERVE_DIR=coverage deno run --allow-all ~/dotfiles/utils/serve_dir.ts
+// PORT=8080 SERVE_DIR=coverage deno run --allow-all ~/dotfiles/utils/serve_dir.ts
 const dir = Deno.env.get("SERVE_DIR");
 Deno.serve(
-  { port: parseInt(Deno.env.get("PORT") || "3001") },
+  { port: parseInt(Deno.env.get("PORT") || "8080") },
   (req: Request) => {
     const pathname = new URL(req.url).pathname;
     if (pathname.startsWith(`/${dir}`)) {
