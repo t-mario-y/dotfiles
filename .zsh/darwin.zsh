2afa8348c4b893c1b0459fde7652a47a6cb8d119
t-mario-y <yoshiaki-tomoi@c-fo.com>
2026-04-16 11:56:29 +0900

    farewell to iTerm2

--- .zsh/darwin.zsh
+++ .zsh/darwin.zsh
@@ -1,6 +1,4 @@
 # macOS共通の.zshrc
-# iTerm
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
 
 if [ "$(uname -p)" = 'arm' ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
